6b6791a4b714d7e2f8c932da690dcb12bc2508e2
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-09-04 21:11:55 -0500

    working homepage

--- config/connection.js
+++ config/connection.js
@@ -3,17 +3,19 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
+// create connection to our database, pass in your MySQL information for the username and password
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  sequelize = new Sequelize ('just_tech_news_db', 'root', '92YankEE123!@#', {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
   });
 }
+
 
 
 
