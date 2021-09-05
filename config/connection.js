30d45ba2277b41b0e66fabf171669833f44a7c5a
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-09-05 17:53:05 -0500

    mysql

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize('just_tech_news_db', 'root', '92YankEE123!@#', {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
