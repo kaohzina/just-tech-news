bb19429a5c21c964c5a6b3a8989d722a273e615d
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-09-05 17:45:14 -0500

    working logins

--- server.js
+++ server.js
@@ -8,6 +8,21 @@ const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001
 
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -17,6 +32,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
+
+
+app.use(session(sess));
 
 //turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
